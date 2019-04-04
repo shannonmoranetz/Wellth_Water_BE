b504fbe6d0f589d698fb8bafe3b53351b24b2c2f
Justin Mauldin <justin.mauldin7@gmail.com>
2019-04-03 22:09:32 -0600

    Creates a route for returning all transactions.

--- wellth_water/urls.py
+++ wellth_water/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import ListUsersView
 from .views import ListEntriesView
+from .views import ListTransactionsView
 
 
 urlpatterns = [
     path('users/', ListUsersView.as_view(), name="users-all"),
-    path('entries/', ListEntriesView.as_view(), name="entries-all")
+    path('entries/', ListEntriesView.as_view(), name="entries-all"),
+    path('transactions/', ListTransactionsView.as_view(), name="transactions-all")
 ]
