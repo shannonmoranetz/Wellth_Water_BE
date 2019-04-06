04e98bb272687b2c87e46c48f32b5cfb2afe2d03
Ben Lee <bendelonlee@gmail.com>
2019-04-06 16:32:29 -0600

    Adds view/action for creating an entry

--- wellth_water/urls.py
+++ wellth_water/urls.py
@@ -3,11 +3,13 @@ from .views import ListUsersView
 from .views import ListEntriesView
 from .views import ListUserEntriesView
 from .views import ListTransactionsView
+from .views import EntriesView
 
 
 urlpatterns = [
     path('users/', ListUsersView.as_view(), name="users-all"),
     path('entries/', ListEntriesView.as_view(), name="entries-all"),
+    path('entries/<int:user_id>/<slug:drinktype>/<int:amount>/', EntriesView.as_view(), name="entries-create"),
     path(r'users/<int:user_id>/entries/', ListUserEntriesView.as_view(), name="user-entries"),
     path('transactions/', ListTransactionsView.as_view(), name="transactions-all"),
 
